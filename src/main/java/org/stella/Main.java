a535f2fb14b4ac582975aea99259fdd42ba8b153
Aziz Vundirov <a.vundirov@innopolis.university>
2026-02-08 14:50:22 +0300

    fix input

--- src/main/java/org/stella/Main.java
+++ src/main/java/org/stella/Main.java
@@ -20,17 +20,18 @@ public class Main
     {
         try
         {
-            Reader input;
-            if (args.length == 0) input = new InputStreamReader(System.in);
-            else input = new FileReader(args[0]);
+            // ИЗМЕНЕНИЕ: Всегда читаем из System.in (stdin)
+            Reader input = new InputStreamReader(System.in);
+
             l = new stellaLexer(new ANTLRInputStream(input));
             l.addErrorListener(new BNFCErrorListener());
         }
         catch(IOException e)
         {
-            System.err.println("Error: File not found: " + args[0]);
+            System.err.println("Error reading from stdin");
             System.exit(1);
         }
+
         p = new stellaParser(new CommonTokenStream(l));
         p.addErrorListener(new BNFCErrorListener());
     }
@@ -38,42 +39,22 @@ public class Main
     public Program parse() throws Exception
     {
         /* The default parser is the first-defined entry point. */
-        /* Other options are: */
-    /* languageDecl, extension, listExtensionName, listExtension, decl,
-       listDecl, localDecl, listLocalDecl, annotation, listAnnotation,
-       paramDecl, listParamDecl, returnType, throwType, expr, listExpr,
-       matchCase, listMatchCase, pattern, listPattern, labelledPattern,
-       listLabelledPattern, binding, listBinding, type, listType,
-       fieldType, listFieldType, typing */
         stellaParser.Start_ProgramContext pc = p.start_Program();
         Program ast = pc.result;
         return ast;
     }
+
     public static void main(String args[]) throws Exception
     {
         Main t = new Main(args);
         try
         {
             Program ast = t.parse();
-//            System.out.println("Pretty printed program:");
-//            System.out.println(PrettyPrinter.print(ast));
 
+            // Запуск проверки типов
             TypeCheck.typecheckProgram(ast);
 
             System.out.println("OK");
-//            if (args.length > 0) {
-//                stellaLexer l;
-//                stellaParser p;
-//                l = new stellaLexer(new ANTLRInputStream(new InputStreamReader(System.in)));
-//                l.addErrorListener(new BNFCErrorListener());
-//                p = new stellaParser(new CommonTokenStream(l));
-//                p.addErrorListener(new BNFCErrorListener());
-//                stellaParser.Start_ExprContext ec = p.start_Expr();
-//                Expr inputExpr = ec.result;
-//                Expr resultExpr = Eval.evalMainWith(ast, inputExpr);
-//
-//                System.out.println(PrettyPrinter.print(resultExpr));
-//            }
         }
         catch(TestError e)
         {
@@ -117,4 +98,3 @@ class BNFCErrorListener implements ANTLRErrorListener
     {
     }
 }
-
